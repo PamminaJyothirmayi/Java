61c71fb0edd5be9c207276ede9e509a726804bed
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-06 12:16:25 +0530

    Create Java_Comments.js

---
+++ JavaLearn/Coding/Java_Comments.js
@@ -0,0 +1,24 @@
+
+//Java Comments
+public class Main {
+  public static void main(String[] args) {
+    // This is single line comment before a line of code
+    System.out.println("Hello World");
+  }
+}
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");  // This is single line comment at the end of a line of code
+  }
+}
+
+
+public class Main {
+  public static void main(String[] args) {
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("Hello World");
+  }
+}
