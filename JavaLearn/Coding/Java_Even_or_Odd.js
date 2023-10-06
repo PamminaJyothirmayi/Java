224aa3c355852713b4cf2343bad16a8575beeffe
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-06 12:22:32 +0530

    Create Java_Even_or_Odd.js

---
+++ JavaLearn/Coding/Java_Even_or_Odd.js
@@ -0,0 +1,18 @@
+//Java How To Find Even or Odd Numbers
+//Check Whether a Number is Even or Odd
+//Find out if a number is even or odd:
+
+
+public class Main {
+  public static void main(String[] args) {
+    int number = 5;
+    
+    if (number % 2 == 0) {
+      System.out.println(number + " is even.");
+    } else {
+      System.out.println(number + " is odd.");
+    }
+  }
+}
+
+//5 is odd.
