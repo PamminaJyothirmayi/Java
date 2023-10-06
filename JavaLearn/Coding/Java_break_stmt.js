bf78c3f31f705bfb1bd3ef3e2609058e00342dbd
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-06 12:23:22 +0530

    Create Java_break_stmt.js

---
+++ JavaLearn/Coding/Java_break_stmt.js
@@ -0,0 +1,21 @@
+
+
+//Java break stament
+
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 4) {
+        break;
+      }
+      System.out.println(i);
+    }  
+  }
+}
+
+/*
+0
+1
+2
+3
+*/
