19e21ea0266240ac90c334d7dbc478ee04859d92
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-06 12:27:39 +0530

    Create Java_Check_Palindrome.js

---
+++ JavaLearn/Coding/Java_Check_Palindrome.js
@@ -0,0 +1,61 @@
+/*****************Palindrome Program in Java*******************/
+//A palindrome number is a number that is same after reverse.
+//For example 545, 151, 34543, 343, 171, 48984 are the palindrome numbers.
+//It can also be a string like LOL, MADAM etc.
+
+
+/*
+Palindrome number algorithm
+-Get the number to check for palindrome
+-Hold the number in temporary variable
+-Reverse the number
+-Compare the temporary number with reversed number
+-If both numbers are same, print "palindrome number"
+-Else print "not palindrome number"
+*/
+
+//Example1:
+class PalindromeExample{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
+//palindrome  number
+
+
+
+
+
+
+//Example2:
+
+import java.util.*;   
+class PalindromeExample2  
+{  
+   public static void main(String args[])  
+   {  
+      String original, reverse = ""; // Objects of String class  
+      Scanner inputUser = new Scanner(System.in);   
+      System.out.println("Enter a string/number to check if it is a palindrome");  
+      original = inputUser.nextLine();   
+      int length = original.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + original.charAt(i);  
+      if (original.equals(reverse))  
+         System.out.println("Entered string/number is a palindrome.");  
+      else  
+         System.out.println("Entered string/number isn't a palindrome.");   
+   }  
+}
